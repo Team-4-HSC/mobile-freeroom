f131f551713dc45fbba93d1c0e897febc945ec14
akmaldira <akmaldiraa@gmail.com>
2022-10-07 23:21:30 +0700

    Nothing is commit LOL

--- lib/app/modules/login/controllers/login_controller.dart
+++ lib/app/modules/login/controllers/login_controller.dart
@@ -2,7 +2,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:freeroom/app/modules/dashboard/views/dashboard_view.dart';
 import 'package:freeroom/app/modules/login/views/login_view.dart';
+import 'package:freeroom/app/modules/splash_screen/views/splash_screen_view.dart';
 import 'package:http/http.dart' as http;
 import 'package:get/get.dart';
 
@@ -40,7 +42,7 @@ class LoginController extends GetxController {
       Get.offAll(() => LoginView());
     } else {
       // if the user exists and logged in the the user is navigated to the Home Screen
-      Get.offAll(() => LoginView());
+      Get.offAll(() => DashboardView());
     }
   }
 
@@ -50,7 +52,7 @@ class LoginController extends GetxController {
       await http
           .get(Uri.parse(
               'https://www.freeroom.app/.netlify/functions/simEndpoint?npm=${npm.text.trim()}&password=${password.text}'))
-          .then((response) {
+          .then((response) async {
         if (response.statusCode != 200) {
           Get.snackbar(
             'Login Error',
@@ -71,7 +73,59 @@ class LoginController extends GetxController {
             ),
           );
         } else {
-          print('Login success handle');
+          try {
+            await auth.signInWithEmailAndPassword(
+              email: '${npm.text.trim()}@gmail.com',
+              password: password.text,
+            );
+          } on FirebaseAuthException catch (e) {
+            if (e.message ==
+                'There is no user record corresponding to this identifier. The user may have been deleted.') {
+              try {
+                await auth.createUserWithEmailAndPassword(
+                  email: '${npm.text.trim()}@gmail.com',
+                  password: password.text,
+                );
+              } on FirebaseAuthException catch (e) {
+                Get.snackbar(
+                  'Gagal Login',
+                  'Firebase Register Error',
+                  backgroundColor: Colors.redAccent,
+                  snackPosition: SnackPosition.TOP,
+                  titleText: Text(
+                    'Gagal Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  messageText: Text(
+                    e.message.toString(),
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                );
+              }
+            }
+            Get.snackbar(
+              'Gagal Login',
+              'Firebase Login Error',
+              backgroundColor: Colors.redAccent,
+              snackPosition: SnackPosition.TOP,
+              titleText: Text(
+                'Gagal Login',
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              messageText: Text(
+                e.message.toString(),
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+            );
+          }
         }
       }).onError((error, stackTrace) {
         print(error);
